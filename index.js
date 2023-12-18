619224b2bbb049dce6d53e8081751a263ee06281
Jxingyu <70691310+Jxingyu@users.noreply.github.com>
2023-12-18 11:21:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = new Koa();
 const net = require("net");
 const router = require("./routes");
 
+
 // 配置信息
 let domain = process.env.ALLOWED_DOMAIN || "*";
 let port = process.env.PORT || 6688;
